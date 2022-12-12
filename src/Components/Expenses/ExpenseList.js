13ef233ae76bf2befc1f6bb4dd0bac987730213a
Manish Kesharwani <mk9934dude@gmail.com>
2022-12-12 09:44:40 +0000

    fetching and posting data from firebase

--- src/Components/Expenses/ExpenseList.js
+++ src/Components/Expenses/ExpenseList.js
@@ -1,14 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ExpenseContext from "../../Store/Expense-Context";
 import ExpenseListItem from "./ExpenseListItem";
 
 const ExpenseList = () => {
+  const [expense, setExpense] = useState([]);
+
   const cntxt = useContext(ExpenseContext);
+
+  useEffect(() => {
+    display();
+  }, []);
+
+  const display = async () => {
+    const data = await fetch(
+      "https://auth-react-b1ea2-default-rtdb.firebaseio.com/expense.json "
+    );
+    const value = await data.json();
+    console.log(value);
+    cntxt.addExpense(value);
+    const array = [];
+    for (const key in value) {
+      array.push({
+        amount: value[key].amount,
+        description: value[key].description,
+        category: value[key].category,
+      });
+    }
+    setExpense(array);
+  };
+  console.log(expense);
+
   return (
     <div>
       <div>Expense List</div>
       <ul>
-        {cntxt.expense.map((item) => (
+        {expense.map((item) => (
           <ExpenseListItem
             key={item.amount + item.description}
             amount={item.amount}
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,42 +1,60 @@
 import React, { useContext, useRef } from "react";
+import "./Expenses.css";
 import ExpenseContext from "../../Store/Expense-Context";
 
 const Expenses = () => {
   const contxt = useContext(ExpenseContext);
-
-  let moneyInputRef = useRef();
+  let amountInputRef = useRef();
   let descInputRef = useRef();
   let listInputRef = useRef();
 
   const submitExpenseHandler = (e) => {
     e.preventDefault();
-    const amount = moneyInputRef.current.value;
+    const amount = amountInputRef.current.value;
     const desc = descInputRef.current.value;
     const list = listInputRef.current.value;
-
     const data = {
       amount: amount,
       description: desc,
       category: list,
     };
-
-    contxt.addExpense(data);
+    fetch("https://auth-react-b1ea2-default-rtdb.firebaseio.com/expense.json", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          alert("Expense Added Successfully..");
+          return resp.json();
+        } else {
+          return resp.json().then((data) => {
+            console.log(data);
+          });
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+    // contxt.addExpense(data);
   };
 
   return (
     <div>
-      <form onSubmit={submitExpenseHandler}>
-        <h1>Daily expenses</h1>
+      <h3 className="secondTitle">Add a new Expense</h3>
 
-        <div>
+      <form onSubmit={submitExpenseHandler}>
+        <div class="formLine left">
           <label>Money Spent</label>
-          <input type="number" ref={moneyInputRef} />
+          <input type="number" ref={amountInputRef} />
         </div>
-        <div>
+        <div class="formLine right">
           <label>Description</label>
           <input type="text" ref={descInputRef} />
         </div>
-        <div>
+        <div class="formLine left">
           <label>Category</label>
           <select ref={listInputRef}>
             <option>Food</option>
@@ -44,7 +62,7 @@ const Expenses = () => {
             <option>Fuel</option>
           </select>
         </div>
-        <button>Add Expense</button>
+        <button class="buttonSave">Add Expense</button>
       </form>
     </div>
   );
